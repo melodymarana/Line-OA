390a895a0ad5c9031360f5a6a5089608dca227b1
melodymarana <melody.marana@gmail.com>
2022-06-09 00:29:18 +0700

    remove config

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './app';
-import { ErrorBoundary } from './bugsnag';
 import { store } from './store';
 import theme from './theme';
 import './assets/css/app.css';
@@ -15,25 +14,19 @@ import '@fontsource/kanit/500.css';
 import '@fontsource/kanit/600.css';
 import '@splidejs/splide/dist/css/splide.min.css';
 
-const ErrorFallback = function ErrorFallback() {
-  return <div>Something went wrong.</div>;
-};
-
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ThemeProvider>
-      </Provider>
-    </ErrorBoundary>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ThemeProvider>
+		</Provider>
+	</React.StrictMode>
 );
