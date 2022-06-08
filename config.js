bec1349a3c8c8065806537bc318f07ab285f9bd7
melodymarana <melody.marana@gmail.com>
2022-06-09 00:26:38 +0700

    Updates

---
+++ config.js
@@ -0,0 +1,7 @@
+(function injectEnv() {
+  window.config = {
+    VITE_LIFF_ID: '',
+    VITE_BUGSNAG_API_KEY: '13fd3578f5669fd58e12ba2e6c1a4eb6',
+    VITE_BUGSNAG_RELEASE_STAGE: 'development',
+  };
+})();
